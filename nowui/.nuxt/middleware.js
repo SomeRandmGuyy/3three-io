2b576150c314066c1b5ae631f9b0dbd5a30c99ea
SomeRandmGuyy <oveshen.govender@gmail.com>
2024-07-04 21:26:12 +0000

    Remove all files for a fresh start

--- nowui/.nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['loading'] = require('../middleware/loading.js')
-middleware['loading'] = middleware['loading'].default || middleware['loading']
-
-export default middleware
